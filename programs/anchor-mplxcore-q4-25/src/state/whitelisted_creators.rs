6ff2f5d1ae5249f41ad7989b30e58ca5e9b8f020
DivineUX23 <divineigbinoba23@gmail.com>
2026-02-10 22:05:38 +0100

    updates

--- programs/anchor-mplxcore-q4-25/src/state/whitelisted_creators.rs
+++ programs/anchor-mplxcore-q4-25/src/state/whitelisted_creators.rs
@@ -16,17 +16,14 @@ impl WhitelistedCreators {
     }
 
     pub fn whitelist_creator(&mut self, creator: &AccountInfo) -> Result<()> {
-        // Check if the array is full
         if self.num_creators as usize >= self.creators.len() {
             return err!(MPLXCoreError::CreatorListFull);
         }
 
-        // Check if already whitelisted
         if self.contains(creator) {
-            return err!(MPLXCoreError::CreatorAlreadyWhitelisted)
+            return err!(MPLXCoreError::CreatorAlreadyWhitelisted);
         }
 
-        // Add the creator at the current num_creators index
         self.creators[self.num_creators as usize] = creator.key();
         self.num_creators += 1;
         Ok(())
