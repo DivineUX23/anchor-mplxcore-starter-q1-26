a4723a49f2cb26bda800233c4c6b39188ccc4d33
DivineUX23 <divineigbinoba23@gmail.com>
2026-02-11 12:09:19 +0100

    freeze working

--- programs/anchor-mplxcore-q4-25/src/instructions/freeze_nft.rs
+++ programs/anchor-mplxcore-q4-25/src/instructions/freeze_nft.rs
@@ -1,20 +1,57 @@
 use anchor_lang::prelude::*;
 use mpl_core::{
-    instructions::UpdatePluginV1CpiBuilder,
-    types::{FreezeDelegate, Plugin},
-    ID as CORE_PROGRAM_ID,
+    ID as CORE_PROGRAM_ID, instructions::UpdatePluginV1CpiBuilder, types::{FreezeDelegate, Plugin}
 };
 
 use crate::{error::MPLXCoreError, state::CollectionAuthority};
 
-// #[derive(Accounts)]
-// pub struct FreezeNft<'info> {
-//    // TODO
-// }
+#[derive(Accounts)]
+pub struct FreezeNft<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
 
-// impl<'info> FreezeNft<'info> {
-//     pub fn freeze_nft(&mut self) -> Result<()> {
-//         // TODO
-//         Ok(())
-//     }
-// }
+    #[account(
+        mut,
+        constraint = asset.owner == &core_program.key()
+    )]
+    pub asset: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+impl<'info> FreezeNft<'info> {
+    pub fn freeze_nft(&mut self) -> Result<()> {
+        let singer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority",
+            &self.collection.key().to_bytes(),
+            &[self.collection_authority.bump]
+        ]];
+
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.payer.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate{frozen: true}))
+            .invoke_signed(singer_seeds)?;
+
+        Ok(())
+    }
+}
